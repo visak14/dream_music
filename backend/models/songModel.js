ecd2272a16e592a1a643575e6b0d8cc164b27875
visak14 <121089328+visak14@users.noreply.github.com>
2024-10-15 20:49:52 +0530

    Update songModel.js

--- backend/models/songModel.js
+++ backend/models/songModel.js
@@ -4,7 +4,7 @@ const songSchema = new mongoose.Schema({
     name: { type: String, required: true },
     description: { type: String, required: true },
     file: { type: String, required: true },  
-    listenr: { type: Number, default: 0 },
+    listeners: { type: Number, default: 0 },
     image: { type: String, required: true }, 
 });
 
